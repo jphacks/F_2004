685e5a4fb17b0c5d65cc5b68a8df19357477be0e
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2020-11-23 09:27:29 +0900

    auto format

--- src/api/user.py
+++ src/api/user.py
@@ -12,16 +12,20 @@ def get_all_users() -> object:
         users = db.session.query(User).all()
     except BaseException as e:
         print(e)
-        return jsonify({"status": "failed", "message": "Error while database session"})
+        return jsonify({"status": "failed",
+                        "message": "Error while database session"})
 
-    response = {"status": "success", "users": [user.to_dict() for user in users]}
+    response = {
+        "status": "success", "users": [
+            user.to_dict() for user in users]}
     return jsonify(response)
 
 
 @api.route("/users", methods=["POST"])
 def add_user() -> object:
     if not ("id" in request.form and "name" in request.form):
-        return jsonify({"status": "failed", "message": "Required argument is missing"})
+        return jsonify({"status": "failed",
+                        "message": "Required argument is missing"})
 
     try:
         user = User(id=int(request.form["id"]), name=request.form["name"])
@@ -29,7 +33,8 @@ def add_user() -> object:
         db.session.commit()
     except BaseException as e:
         print(e)
-        return jsonify({"status": "failed", "message": "Error while database session"})
+        return jsonify({"status": "failed",
+                        "message": "Error while database session"})
 
     response = {"status": "success", "user": user.to_dict()}
     return jsonify(response)
