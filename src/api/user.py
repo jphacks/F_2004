cee2ce22c92c7f554e96f4e88c916b4db4071a4e
admin <c211587r@icu.ac.jp>
2020-11-28 05:56:58 +0900

    多分完成

--- src/api/user.py
+++ src/api/user.py
@@ -52,19 +52,6 @@ def add_user() -> object:
     return jsonify(response)
 
 
-@api.route("/users/<int:user_id>", methods=["GET"])
-def get_user(user_id: int) -> object:
-    try:
-        user = db.session.query(User).filter(User.id == user_id).first()
-    except BaseException as e:
-        print(e)
-        return jsonify({"status": "failed",
-                        "message": "Error while database session"})
-
-    response = {"status": "success", "user": user.to_dict()}
-    return jsonify(response)
-
-
 @api.route("/users/group/<int:group_id>", methods=["GET"])
 def get_group_users(group_id: int) -> object:
     try:
@@ -88,12 +75,12 @@ def test() -> object:
         split_item = item.split("=")
         list[split_item[0]] = split_item[1]
 
-    user_id = list["text"].split("+")[0]
-
-    user = db.session.query(User).filter(User.id == user_id).first()
-    user.is_watch = True
-
-    db.session.commit()
+    user_names = list["text"].split("+")
+    print(user_names)
+    for user_name in user_names:
+        user = db.session.query(User).filter(User.name == user_name).first()
+        user.is_watch = True
+        db.session.commit()
 
     response = {
         "text": "ユーザーのセットが完了しました！",
@@ -106,24 +93,41 @@ def test() -> object:
 @api.route("/test2", methods=["GET"])
 def post_notification():
     post_url = 'https://slack.com/api/chat.postMessage'
+    msg = "@やっすーさん！そろそろコーヒーでも飲みませんか？"
     payload = {
-        'token': "xoxb-1506483628352-1476246434710-AKQT2LbfRb29LtsT4UugbsiE",
-        "channel_name": "#general",
-        "user_name": "t.watanabe0452",
-        "text": "Hello Flask",
+        # 'token': "xoxb-1506483628352-1476246434710-AKQT2LbfRb29LtsT4UugbsiE",
+        'token':'xoxb-1506483628352-1476246434710-awhAYeScSKJplDE0hb5Xjw9A',
+        "channel": "#general",
+        "text": msg,
         "scope": "chat:write:bot"
     }
 
     res = requests.post(post_url, payload)
-    print(res.status_code, res.text)
+    # print(payload)
+    # print(res.status_code, res.text)
 
-    return jsonify("hello")
+    # return jsonify("hello")
 
 
 @api.route("/test3", methods=["POST"])
 def test_3():
+    body = request.get_data().decode()
+    print(body)
+
+    list = {}
+    for item in body.split("&"):
+        split_item = item.split("=")
+        list[split_item[0]] = split_item[1]
+
+    user_names = list["text"].split("+")
+    print(user_names)
+    for user_name in user_names:
+        user = db.session.query(User).filter(User.name == user_name).first()
+        user.is_watch = False
+        db.session.commit()
+
     response = {
-        "text": "ユーザーのセットを解除しました！",
+        "text": "ユーザーのセットを解除しました",
         "response_type": "in_channel"
     }
 
---
+++ src/predict.py
@@ -0,0 +1,79 @@
+import schedule
+import numpy
+from sqlalchemy import desc
+from src.model import *
+
+def train(results):
+    results = results.clear()
+    
+    # is_wathing==TrueのユーザーIDをリストに格納
+    watching_user_id = db.session.query(User.id).filter(User.is_watch).all()
+    print(watching_user_id)
+    watching_list = []
+    for w in watching_user_id:
+        w = int(w[0])
+        print(w)
+        print(type(w))
+        watching_list.append(w)
+    
+    # watching_userの集中力をそれぞれ取得
+    watching_users_cv = {}
+    for i in watching_list:
+        cv_list = []
+        watching_user_cv = db.session.query(ConcentrationValue.concentration_value)\
+            .filter(ConcentrationValue.user_id == i)\
+            .order_by(desc(ConcentrationValue.created_at)) \
+            .all()
+        for w in watching_user_cv:
+            w = int(w[0])
+            cv_list.append(w)
+        watching_users_cv[i] = cv_list
+        print(watching_users_cv) 
+
+    #ユーザーごとに、直前６個の平均と標準偏差を計算
+    results = []
+    for watching_user_cv in watching_users_cv.items():
+        try:
+            # print(values)
+            user_pre_cv = []
+            user_id = watching_user_cv[0]
+            values = watching_user_cv[1]
+            for i in range(1,len(values)):
+                if values[i] <= 4 and values[i-1] > 4:
+                    user_pre_cv.append(values[i-6:i])#本当は[i-36:i-30]+エラー処理が必要
+            user_pre_cv_array = np.array(user_pre_cv)#行列に変換
+            # print(user_pre_cv_array)
+            means = np.round(user_pre_cv_array.mean(axis=0),2)#直前6個の平均
+            means = means.tolist()
+            sds = np.round(user_pre_cv_array.std(axis=0),2)#直線6個の標準偏差
+            sds = sds.tolist()
+        except:
+            print("デフォルトの予測を使用します")
+            means = [5,5,5,6,6,6]
+            sds = [0,0,1,1,1,1]
+
+        r = []
+        r.append(means)
+        r.append(sds)
+        results[user_id]=r
+
+
+
+results = {}
+# #初期値をセット（最大10人分対応可能）
+# for n in range(10):
+#     means = [5,5,5,6,6,6]
+#     sds = [0,0,1,1,1,1]
+#     r = []
+#     r.append(means)
+#     r.append(sds)
+#     results.append(r)
+
+#30分おきに、値をアップデート
+try:
+    schedule.every(30).minutes.do(train(results))
+except:
+    results = {}
+
+def main():
+    return results
